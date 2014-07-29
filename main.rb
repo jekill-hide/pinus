3bf1c5726d8c8a89ec2d712d85b9c9d68b20e8df
jekill-hide <azeyev.gleb@mail.ru>
2014-07-29 16:41:29 +0400

    main.rb had changed

--- main.rb
+++ main.rb
@@ -1,11 +1,26 @@
 #!/usr/bin/env ruby
 
 require 'active_record'
+require 'sqlite3'
 require '/home/jekill-hide/ruby-lessons/pinus/lib/record_class'
 
+ActiveRecord::Base.establish_connection(
+	:adapter => 'sqlite3',
+	:database => 'users.db'
+)
+
+class Admin < ActiveRecord::Base
+end
+
+Admin.create(
+	login: 'lenin'
+	password: '1918'
+)
+
 puts "login:"
-gets user_login
+user_login = gets.chomp
 
 puts "password"
-gets user_pass
+user_pass = gets.chomp
+
 
